f8110e4d988feb0067ef24583874a146b41310e4
Chapman Nils, GHR-OSA-NEX-15 <Nils.Chapman@swisscom.com>
2018-12-01 15:52:37 +0100

    Function um R aus U und P berechnen Implementiert.

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -61,6 +61,11 @@ public class Calculator {
 	public double iAusUundR(double u,double r){
 		return u/r;
 	}
+	
+	//Feature 4
+		public double rAusUundP(double u,double p){
+			return (u*u)/p;
+		}
 
 	
 	
