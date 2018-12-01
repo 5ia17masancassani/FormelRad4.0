17ae60a4351aca42f273c65bf5063b68a18daa36
5ia17masancassani <m.sancassani@hispeed.ch>
2018-12-01 15:26:43 +0100

    Calculate U und R

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -94,6 +94,15 @@ public class Calculator {
 
 		}
 		
+		if (leistung != 0.0 && spannung == 0.0 && strom != 0.0 && widerstand == 0.0) {
+
+			spannung = Math.round(uAusPundI(leistung, strom) * 100.0) / 100.0;
+		
+			widerstand = Math.round(rAusPundI(leistung, strom) * 100.0) / 100.0;
+			
+
+		}
+		
 
 	}
 
